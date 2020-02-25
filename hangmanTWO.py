0bbe0eded318cb0c7a7fad27e7e651fb05d5283a
nbabal20 <60886328+nbabal20@users.noreply.github.com>
2020-02-25 08:58:21 -0500

    Add files via upload

---
+++ hangmanTWO.py
@@ -0,0 +1,28 @@
+letter=""
+secret=""
+updatedWord = []
+
+def initialize():
+    word=("secret")
+
+def getLetter():
+    global letter 
+
+def ifWon():
+    getLetter()
+    pos = secret.rfind(letter)
+    uw[pos]=letter
+    print uw=[]
+    
+def test():
+    global updatedWord
+    life = 6
+    if updatedWord == secret:
+        ifWon()
+    elif
+        lives=life-1
+        return lives
+        
+        
+    else life = 0:
+        print=("you lost")         
